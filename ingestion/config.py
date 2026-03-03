93619b0b4b4cb4d01bd5be504e9dadb2928a1d61
Michael Isikima <isikima.mi@gmail.com>
2026-03-03 10:56:12 +0000

    configuration file add containing key variables

---
+++ ingestion/config.py
@@ -0,0 +1,5 @@
+
+API_BASE_URL = "https://data.cityofnewyork.us/resource/erm2-nwe9.json"
+BATCH_SIZE = 1000                                                           # number of records per API call
+LAST_RUN_FILE = "last_run_timestamp.txt"
+LOCAL_BRONZE_DIR = "local_bronze/"
